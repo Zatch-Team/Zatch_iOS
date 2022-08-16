6981753fe6d8c128c3c8e9ef01b27690eb711148
Ssooya <bsy24660@naver.com>
2022-08-16 15:39:10 +0900

    [feat]:ChattingVC HiddenInfoView UI 추가

--- Zatch/Source/Domain/Chatting/Cell/HiddenZatchInfoView.swift
+++ Zatch/Source/Domain/Chatting/Cell/HiddenZatchInfoView.swift
@@ -8,14 +8,125 @@
 import UIKit
 
 class HiddenZatchInfoView: UIView {
+    
+    let productLabel = UILabel().then{
+        $0.text = "다른 코스튬 아무거나"
+        $0.font = UIFont.pretendard(size: 14, family: .Medium)
+        $0.textColor = .white
+    }
+    
+    let eclipseView = UIView().then{
+        $0.backgroundColor = .white
+        $0.layer.cornerRadius = 1
+    }
+    
+    let eclipseBackView = UIView().then{
+        $0.backgroundColor = .transparent
+    }
+    
+    let locationLabel = UILabel().then{
+        $0.text = "서대문구 홍제동"
+        $0.font = UIFont.pretendard(size: 12, family: .Medium)
+        $0.textColor = .white
+    }
+    
+    let dateLabel = UILabel().then{
+        $0.text = "21.07.05"
+        $0.font = UIFont.pretendard(size: 13, family: .Medium)
+        $0.textColor = .white
+    }
+    
+    let timeLabel = UILabel().then{
+        $0.text = "00시00분"
+        $0.font = UIFont.pretendard(size: 13, family: .Medium)
+        $0.textColor = .white
+    }
+    
+    let remainAppointmentLabel = UILabel().then{
+        $0.text = "1일 남음"
+        $0.font = UIFont.pretendard(size: 13, family: .Medium)
+        $0.textColor = .white
+    }
+    
+    let productImage = UIImageView().then{
+        $0.layer.cornerRadius = 8
+        $0.clipsToBounds = true
+        $0.backgroundColor = .black10
+    }
+    
+    let topStackView = UIStackView().then{
+        $0.spacing = 4
+        $0.axis = .horizontal
+    }
+    
+    let bottomStackView = UIStackView().then{
+        $0.spacing = 12
+        $0.axis = .horizontal
+    }
+    
+    let infoStackView = UIStackView().then{
+        $0.spacing = 11
+        $0.axis = .vertical
+        $0.alignment = .leading
+        
+    }
 
     override init(frame: CGRect) {
         super.init(frame: frame)
         
         self.backgroundColor = .zatchPurple
         
+        self.layer.maskedCorners = [.layerMaxXMaxYCorner, .layerMaxXMinYCorner]
+        self.layer.cornerRadius = 80/2
+        self.layer.masksToBounds = true
+        
+        self.addSubview(infoStackView)
+        self.addSubview(productImage)
+        
+        self.eclipseBackView.addSubview(eclipseView)
+        
+        infoStackView.addArrangedSubview(topStackView)
+        infoStackView.addArrangedSubview(bottomStackView)
+        
+        topStackView.addArrangedSubview(productLabel)
+        topStackView.addArrangedSubview(eclipseBackView)
+        topStackView.addArrangedSubview(locationLabel)
+        
+        bottomStackView.addArrangedSubview(dateLabel)
+        bottomStackView.addArrangedSubview(timeLabel)
+        bottomStackView.addArrangedSubview(remainAppointmentLabel)
+        
         self.snp.makeConstraints{ make in
             make.height.equalTo(80)
+        }
+        
+        infoStackView.snp.makeConstraints{ make in
+            make.top.equalToSuperview().offset(16)
+            make.leading.equalToSuperview().offset(24)
+            make.bottom.equalToSuperview().offset(-20)
+            make.trailing.equalToSuperview().offset(-95)
+        }
+        
+        topStackView.snp.makeConstraints{ make in
+            make.height.equalTo(17)
+        }
+        
+        productImage.snp.makeConstraints{ make in
+            make.top.equalToSuperview().offset(15)
+            make.bottom.equalToSuperview().offset(-13)
+            make.trailing.equalToSuperview().offset(-28)
+            make.width.height.equalTo(52)
+        }
+
+        eclipseBackView.snp.makeConstraints{ make in
+            make.width.equalTo(2)
+            make.height.equalTo(17)
+        }
+        
+        eclipseView.snp.makeConstraints{ make in
+            make.width.height.equalTo(2)
+            make.leading.trailing.equalToSuperview()
+            make.centerY.equalToSuperview()
         }
     }
     
