509006978ca3591dc7932923a659bd35b0f83bfa
Ssooya <bsy24660@naver.com>
2022-09-11 15:37:22 +0900

    #39: /global/Resource/extension 파일 정리

--- Zatch/global/Resource/extenstion/UIImageView.swift
+++ Zatch/global/Resource/extenstion/UIImageView.swift
@@ -8,10 +8,10 @@
 import UIKit
 
 extension UIImageView {
-
-func setImageTintColor(_ color: UIColor) {
-    let tintedImage = self.image?.withRenderingMode(.alwaysTemplate)
-    self.image = tintedImage
-    self.tintColor = color
-  }
+    
+    func setImageTintColor(_ color: UIColor) {
+        let tintedImage = self.image?.withRenderingMode(.alwaysTemplate)
+        self.image = tintedImage
+        self.tintColor = color
+    }
 }
