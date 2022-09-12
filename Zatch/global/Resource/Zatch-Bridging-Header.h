dde82ee95c6319ffe4f21911dc36c34e47c9adc8
Ssooya <bsy24660@naver.com>
2022-09-12 15:15:29 +0900

    #43: 카카오 지도 SDK 파일 추가 및 Zatch_Bridging_Header 파일에 카카오 지도 관련 코드 추가

--- Zatch/global/Resource/Zatch-Bridging-Header.h
+++ Zatch/global/Resource/Zatch-Bridging-Header.h
@@ -5,4 +5,11 @@
 //  Created by 박지윤 on 2022/09/11.
 //
 
+
+#ifndef Zatch_Bridging_Header_h
+#define Zatch_Bridging_Header_h
+
 #import "UISheetPresentationControllerDetent+Private.h"
+#import <DaumMap/MTMapView.h>
+
+#endif /* Zatch_Bridging_Header_h */
