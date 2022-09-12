fe405e9607376a1c4f8dee3c29a4f98aa41fd748
Ssooya <bsy24660@naver.com>
2022-09-12 17:24:41 +0900

    #43: 사용자 위치 설정VC에서 내 동네 태그 collectionView Cell 파일 추가

---
+++ Zatch/domain/Cells/TownCells/MyTownTagCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  MyTownTagCollectionViewCell.swift
+//  Zatch
+//
+//  Created by 박지윤 on 2022/09/12.
+//
+
+import UIKit
+
+class MyTownTagCollectionViewCell: UICollectionViewCell {
+    
+}
