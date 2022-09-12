b2c7648967aa02cd50bd444b0f8c7d9d005f8839
Ssooya <bsy24660@naver.com>
2022-09-12 12:15:36 +0900

    #43: 동네인증 파일 추가

---
+++ Zatch/domain/ViewControllers/Town/MapTownViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TownMapViewController.swift
+//  Zatch
+//
+//  Created by 박지윤 on 2022/09/12.
+//
+
+import UIKit
+
+class MapTownViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Zatch/domain/Views/TownView/SplashTownView.swift
@@ -0,0 +1,20 @@
+//
+//  TownSplashView.swift
+//  Zatch
+//
+//  Created by 박지윤 on 2022/09/12.
+//
+
+import UIKit
+
+class SplashTownView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
