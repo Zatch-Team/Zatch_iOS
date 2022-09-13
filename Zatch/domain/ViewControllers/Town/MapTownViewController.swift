b67af02a001eb967e17086aa04d989f8badd924c
Ssooya <bsy24660@naver.com>
2022-09-13 11:33:56 +0900

    #43: 카카오 지도 터치 이벤트 시 커스텀 마커 표시 기능

--- Zatch/domain/ViewControllers/Town/MapTownViewController.swift
+++ Zatch/domain/ViewControllers/Town/MapTownViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class MapTownViewController: UIViewController {
     
+    //MARK: - Properties
+    var mapMarker = CustomMapMarker()
+    
     let mainView = MapTownView()
 
     override func viewDidLoad() {
@@ -27,5 +30,36 @@ class MapTownViewController: UIViewController {
 }
 
 extension MapTownViewController: MTMapViewDelegate{
+    
+    /*
+     //1. 지도 상에서 특정 위치 터치 -> singleTapOnMapPoint
+     //2. 선택한 위치에 마커 표시 -> addPOIItem (1개만 표시되도록 설정)
+     
+     3. 재치 시작하기 버튼 클릭시, 마커 위치 정보 통해 '동' 정보 추출하기
+     
+     추가. customDirectionImageAnchorPointOffset로 마커 중심 변경하기
+     */
+    
+    class CustomMapMarker: MTMapPOIItem{
+        
+        override init(){
+            
+            super.init()
+            
+            self.showAnimationType = .springFromGround
+            self.markerType = .customImage
+            self.customImage = UIImage(named: "map_marker") //map_marker
+        }
+        
+    }
+    
+    func mapView(_ mapView: MTMapView!, singleTapOn mapPoint: MTMapPoint!) {
+        print("click event")
+        
+        self.mapMarker.mapPoint = mapPoint
+        mapView.remove(self.mapMarker)
+        mapView.addPOIItems([self.mapMarker])
+        
+    }
     
 }
