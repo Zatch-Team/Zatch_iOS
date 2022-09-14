3f40ff3f080cd2c958061043da3123e0f67344f6
Ssooya <bsy24660@naver.com>
2022-09-15 08:42:54 +0900

    #57: 차단된 사용자 페이지 관련 파일(BlockUser) 추가

---
+++ Zatch/domain/Cells/MypageCells/MyPage/BlockUserTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  BlockUserTableViewCell.swift
+//  Zatch
+//
+//  Created by 박지윤 on 2022/09/15.
+//
+
+import UIKit
+
+class BlockUserTableViewCell: BaseTableViewCell {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
