13037a9a6a9e07421b99aefbb626edd106168bca
Ssooya <bsy24660@naver.com>
2022-09-19 15:41:51 +0900

    #66: 설정 페이지 UI 구현

--- Zatch/domain/Cells/MypageCells/Setting/AlarmSettingTableViewCell.swift
+++ Zatch/domain/Cells/MypageCells/Setting/AlarmSettingTableViewCell.swift
@@ -7,14 +7,54 @@
 
 import UIKit
 
-class AlarmSettingTableViewCell: UIView {
+class AlarmSettingTableViewCell: BaseTableViewCell {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    static let cellIdentifier = "AlarmSettingTableViewCell"
+    
+    let titleLabel = UILabel().then{
+        $0.font = UIFont.pretendard(size: 15, family: .Medium)
+        $0.textColor = .black85
     }
-    */
-
+    
+    let explainLabel = UILabel().then{
+        $0.numberOfLines = 0
+        $0.textAlignment = .left
+        $0.font = UIFont.pretendard(size: 12, family: .Regular)
+        $0.textColor = .black85
+        $0.setTextWithLineHeight(lineHeight: 15.6)
+        
+    }
+    
+    let alarmSwitch = UISwitch()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        self.baseView.addSubview(titleLabel)
+        self.baseView.addSubview(explainLabel)
+        self.baseView.addSubview(alarmSwitch)
+        
+        titleLabel.snp.makeConstraints{
+            $0.top.equalToSuperview().offset(10)
+            $0.leading.equalToSuperview().offset(20)
+        }
+        
+        explainLabel.snp.makeConstraints{
+            $0.top.equalTo(titleLabel.snp.bottom)
+            $0.leading.equalTo(titleLabel)
+            $0.trailing.equalToSuperview().offset(-90)
+            $0.bottom.equalToSuperview().offset(-10)
+        }
+        
+        alarmSwitch.snp.makeConstraints{
+            $0.top.equalToSuperview().offset(10.5)
+            $0.leading.equalTo(explainLabel.snp.trailing).offset(21)
+            $0.trailing.equalToSuperview().offset(-20)
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
 }
--- Zatch/domain/ViewControllers/Mypage/Mypage/SettingViewController.swift
+++ Zatch/domain/ViewControllers/Mypage/Mypage/SettingViewController.swift
@@ -7,14 +7,104 @@
 
 import UIKit
 
-class SettingViewController: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+class SettingViewController: BaseCenterTitleViewController {
+    
+    let mainView = SettingView()
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        setUpInitSetting()
     }
-    */
+    
+    func setUpInitSetting(){
+        navigationTitle.text = "설정"
+        
+        self.view.addSubview(mainView)
+        
+        mainView.snp.makeConstraints{
+            $0.top.leading.bottom.trailing.equalToSuperview()
+        }
+        
+        mainView.tableView.delegate = self
+        mainView.tableView.dataSource = self
+        
+    }
 
+}
+
+extension SettingViewController: UITableViewDataSource, UITableViewDelegate{
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        switch section {
+        case 0:
+            return 3
+        case 1:
+            return 2
+        case 2:
+            return 3
+        default:
+            return 0
+        }
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+
+        switch indexPath.section{
+        case 0:
+            switch indexPath.row{
+            case 0:
+                guard let cell = tableView.dequeueReusableCell(withIdentifier: SettingTitleTableViewCell.cellIdentifier, for: indexPath) as? SettingTitleTableViewCell else { fatalError() }
+                cell.titleLabel.text = "알림 설정"
+                return cell
+            case 1:
+                guard let cell = tableView.dequeueReusableCell(withIdentifier: AlarmSettingTableViewCell.cellIdentifier, for: indexPath) as? AlarmSettingTableViewCell else { fatalError() }
+                cell.titleLabel.text = "채팅 알림"
+                cell.explainLabel.text = "누군가 나에게 채팅을 보낼 경우에 알림을 받습니다."
+                return cell
+            case 2:
+                guard let cell = tableView.dequeueReusableCell(withIdentifier: AlarmSettingTableViewCell.cellIdentifier, for: indexPath) as? AlarmSettingTableViewCell else { fatalError() }
+                cell.titleLabel.text = "가치 알림"
+                cell.explainLabel.text = "내가 올린 가치에 참여자가 생기거나, 내가 참여한 가치의 참여자가 모두 모였을 경우에 알림을 받습니다."
+                return cell
+            default:
+                break
+            }
+        case 1:
+            if (indexPath.row == 0){
+                guard let cell = tableView.dequeueReusableCell(withIdentifier: SettingTitleTableViewCell.cellIdentifier, for: indexPath) as? SettingTitleTableViewCell else { fatalError() }
+                cell.titleLabel.text = "사용자 설정"
+                return cell
+            }else{
+                guard let cell = tableView.dequeueReusableCell(withIdentifier: DefaultSettingTableViewCell.cellIdentifier, for: indexPath) as? DefaultSettingTableViewCell else { fatalError() }
+                cell.titleLabel.text = "차단된 사용자"
+                return cell
+            }
+        case 2:
+            switch indexPath.row{
+            case 0:
+                guard let cell = tableView.dequeueReusableCell(withIdentifier: SettingTitleTableViewCell.cellIdentifier, for: indexPath) as? SettingTitleTableViewCell else { fatalError() }
+                cell.titleLabel.text = "계정"
+                return cell
+            case 1:
+                guard let cell = tableView.dequeueReusableCell(withIdentifier: DefaultSettingTableViewCell.cellIdentifier, for: indexPath) as? DefaultSettingTableViewCell else { fatalError() }
+                cell.titleLabel.text = "회원 탈퇴"
+                return cell
+            case 2:
+                guard let cell = tableView.dequeueReusableCell(withIdentifier: DefaultSettingTableViewCell.cellIdentifier, for: indexPath) as? DefaultSettingTableViewCell else { fatalError() }
+                cell.titleLabel.text = "로그아웃"
+                return cell
+            default:
+                break
+            }
+        default:
+            break
+        }
+        return BaseTableViewCell()
+    }
 }
