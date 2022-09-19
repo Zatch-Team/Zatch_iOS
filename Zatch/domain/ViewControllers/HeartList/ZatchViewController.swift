ecac80ae54dc153ee39e5764a0227f2bc4b46c51
gom1n <alice436777@gmail.com>
2022-09-19 20:17:10 +0900

    #74: 재치 관심목록 페이지 테이블뷰 구현

--- Zatch/domain/ViewControllers/HeartList/ZatchViewController.swift
+++ Zatch/domain/ViewControllers/HeartList/ZatchViewController.swift
@@ -8,17 +8,62 @@
 import UIKit
 
 class ZatchViewController: UIViewController {
+    var heartZatchTableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let label = UILabel().then{
-            $0.text = "Zatch"
-        }
-        self.view.addSubview(label)
-        label.snp.makeConstraints { make in
-            make.centerY.centerX.equalToSuperview()
-        }
+        setUpTableView()
     }
 
+}
+extension ZatchViewController {
+    func setUpTableView() {
+        heartZatchTableView = UITableView().then{
+            $0.delegate = self
+            $0.dataSource = self
+            $0.register(MyZatchTableViewCell.self, forCellReuseIdentifier: "MyZatchTableViewCell")
+            
+            // autoHeight
+            $0.rowHeight = UITableView.automaticDimension
+            $0.estimatedRowHeight = UITableView.automaticDimension
+            $0.showsVerticalScrollIndicator = false
+        }
+        self.view.addSubview(heartZatchTableView)
+        heartZatchTableView.snp.makeConstraints { make in
+            make.leading.trailing.top.bottom.equalToSuperview()
+        }
+    }
+}
+// MARK: - TableView delegate
+extension ZatchViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "MyZatchTableViewCell", for: indexPath) as? MyZatchTableViewCell else { return UITableViewCell() }
+        
+        addHeartToCell(cell)
+        return cell
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 138 + 12
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+extension ZatchViewController {
+    func addHeartToCell(_ cell: MyZatchTableViewCell) {
+        let heartButton = UIButton().then{
+            $0.setImage(UIImage(named: "filledHeart"), for: .selected)
+            $0.setImage(UIImage(named: "heart_silver"), for: .normal)
+        }
+        cell.contentView.addSubview(heartButton)
+        heartButton.snp.makeConstraints { make in
+            make.width.height.equalTo(24)
+            make.trailing.top.equalToSuperview().inset(16)
+        }
+    }
 }
