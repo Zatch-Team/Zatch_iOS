8cd1f5633c6fa5777c2a7cf2a02cd4d649f27dde
Ssooya <bsy24660@naver.com>
2022-09-19 14:07:37 +0900

    #66: 마이페이지 설정, 회원탈퇴 파일 추가

---
+++ Zatch/domain/ViewControllers/Mypage/Mypage/UserLeaveViewController.swift
@@ -0,0 +1,20 @@
+//
+//  UserLeaveViewController.swift
+//  Zatch
+//
+//  Created by 박지윤 on 2022/09/19.
+//
+
+import UIKit
+
+class UserLeaveViewController: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
