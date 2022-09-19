dd589f46c0061d62dde8148c91300db4271a23c5
gom1n <alice436777@gmail.com>
2022-09-19 15:22:55 +0900

    close #68: 프로필 편집 페이지 UI 구현 (오른쪽 상단 수정 버튼 x)

---
+++ Zatch/domain/Views/MypageView/MyProfile/ModifyProfileView.swift
@@ -0,0 +1,92 @@
+//
+//  ModifyProfileView.swift
+//  Zatch
+//
+//  Created by gomin on 2022/09/19.
+//
+
+import Foundation
+import UIKit
+
+class ModifyProfileView: UIView {
+    // MARK: - Views
+    let userImage = UIImageView().then{
+        $0.backgroundColor = .systemGray4
+        $0.layer.cornerRadius = 50
+    }
+    let cameraButton = UIButton().then{
+        $0.setImage(UIImage(named: "cameraButton"), for: .normal)
+    }
+    let modifyTextfield = UITextField().then {
+        $0.borderStyle = .none
+        $0.backgroundColor = .white
+        $0.addPadding(left: 28, right: 28)
+        $0.placeholder = "닉네임"
+        $0.font = UIFont.pretendard(size: 16, family: .Bold)
+        $0.setClearButton(with: UIImage(named: "cancel")!, mode: .whileEditing)
+    }
+    let textfieldUnderline = UIView().then{
+        $0.backgroundColor = .black5
+    }
+    let message = UILabel().then{
+        $0.text = "*닉네임을 변경하면 30일 동안 추가 변경 불가합니다."
+        $0.font = UIFont.pretendard(size: 12, family: .Regular)
+        $0.numberOfLines = 0
+        
+        $0.asColor(targetString: "30일 동안 추가 변경 불가", color: .red)
+    }
+    
+    // MARK: - Life Cycles
+    var viewcontroller: ModifyProfileViewController!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setUpView()
+        setUpConstraint()
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    // MARK: - Functions
+    func setViewController(_ vc: ModifyProfileViewController) {
+        self.viewcontroller = vc
+    }
+    func setUpView() {
+        addSubview(userImage)
+        userImage.addSubview(cameraButton)
+        
+        addSubview(modifyTextfield)
+        modifyTextfield.addSubview(textfieldUnderline)
+        
+        addSubview(message)
+    }
+    func setUpConstraint() {
+        userImage.snp.makeConstraints { make in
+            make.width.height.equalTo(100)
+            make.centerX.equalToSuperview()
+            make.top.equalToSuperview().offset(20)
+        }
+        cameraButton.snp.makeConstraints { make in
+            make.width.height.equalTo(35)
+            make.trailing.equalTo(userImage)
+            make.bottom.equalTo(userImage.snp.bottom).offset(4)
+        }
+        modifyTextfield.snp.makeConstraints { make in
+            make.height.equalTo(44)
+            make.leading.trailing.equalToSuperview().inset(20)
+            make.top.equalTo(userImage.snp.bottom).offset(36)
+            make.centerX.equalToSuperview()
+        }
+        textfieldUnderline.snp.makeConstraints { make in
+            make.height.equalTo(1)
+            make.leading.equalToSuperview().inset(20)
+            make.trailing.equalToSuperview()
+            make.bottom.equalToSuperview().offset(-8)
+        }
+        message.snp.makeConstraints { make in
+            make.top.equalTo(modifyTextfield.snp.bottom).offset(4)
+            make.leading.trailing.equalTo(modifyTextfield).inset(20)
+        }
+    }
+}
--- Zatch/global/Resource/extenstion/UITextField.swift
+++ Zatch/global/Resource/extenstion/UITextField.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 extension UITextField {
-    
+    // MARK: Set placeholder
     func setPlaceholderColor(_ placeholderColor: UIColor) {
         attributedPlaceholder = NSAttributedString(
             string: placeholder ?? "",
@@ -18,7 +18,7 @@ extension UITextField {
             ].compactMapValues { $0 }
         )
     }
-    
+    // MARK: Add padding
     func addPadding(left: CGFloat = 6, right: CGFloat = 6) {
         let leftPaddingView = UIView(frame: CGRect(x: 0, y: 0, width: left, height: self.frame.height))
         let rightPaddingView = UIView(frame: CGRect(x: 0, y: 0, width: right, height: self.frame.height))
@@ -28,4 +28,25 @@ extension UITextField {
         self.rightViewMode = ViewMode.always
     }
     
+    // MARK: Clear button Custom
+    func setClearButton(with image: UIImage, mode: UITextField.ViewMode) {
+        let clearButton = UIButton(type: .custom)
+        clearButton.setImage(image, for: .normal)
+        clearButton.frame = CGRect(x: 0, y: 0, width: 40, height: 40)
+        clearButton.contentMode = .scaleAspectFit
+        clearButton.addTarget(self, action: #selector(UITextField.clear(sender:)), for: .touchUpInside)
+        self.addTarget(self, action: #selector(UITextField.displayClearButtonIfNeeded), for: .editingDidBegin)
+        self.addTarget(self, action: #selector(UITextField.displayClearButtonIfNeeded), for: .editingChanged)
+        self.rightView = clearButton
+        self.rightViewMode = mode
+    }
+    @objc
+    private func displayClearButtonIfNeeded() {
+        self.rightView?.isHidden = (self.text?.isEmpty) ?? true
+    }
+    @objc
+    private func clear(sender: AnyObject) {
+        self.text = ""
+        sendActions(for: .editingChanged)
+    }
 }
