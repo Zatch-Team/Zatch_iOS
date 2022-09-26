6de68c712b90df9af431c3eb1e82243edd98bc1e
Ssooya <bsy24660@naver.com>
2022-09-26 09:14:47 +0900

    close #85: BaseVC 오른쪽 상단 버튼 추가 및 버튼 클릭 이벤트 함수 구현

--- Zatch/global/Source/BaseViewController.swift
+++ Zatch/global/Source/BaseViewController.swift
@@ -8,7 +8,28 @@
 import UIKit
 
 class BaseViewController: UIViewController {
-
+    
+    init(){
+        super.init(nibName: nil, bundle: nil)
+        self.viewDidLoad()
+    }
+    
+    init(title: String){
+        super.init(nibName: nil, bundle: nil)
+        self.viewDidLoad()
+        self.rightPositionBtn = EtcButton(title: title)
+    }
+    
+    init(btnImage: UIImage){
+        super.init(nibName: nil, bundle: nil)
+        self.viewDidLoad()
+        self.rightPositionBtn = EtcButton(image: btnImage)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
     let navigationView = UIView()
     
     lazy var backBtn = UIButton().then{
@@ -19,6 +40,12 @@ class BaseViewController: UIViewController {
     
     lazy var navigationTitle = UILabel().then{
         $0.font = UIFont.pretendard(size: 16, family: .Bold)
+    }
+    
+    lazy var rightPositionBtn: EtcButton? = nil{
+        didSet{
+            setRightPositionBtn()
+        }
     }
 
     override func viewDidLoad() {
@@ -41,16 +68,60 @@ class BaseViewController: UIViewController {
         }
         
         backBtn.snp.makeConstraints{ make in
-            make.width.height.equalTo(24)
-            make.leading.equalToSuperview().offset(16)
+            make.width.equalTo(backBtn.snp.height)
+            make.leading.equalToSuperview().offset(20)
+            make.top.equalToSuperview().offset(16)
             make.centerY.equalToSuperview()
         }
     }
     
-    @objc
-    func backBtnDidClicked(){
+    @objc func backBtnDidClicked(){
         self.navigationController?.popViewController(animated: true)
     }
+    
+    @objc func rightPositionBtnDidClicked(){
+        print("right position btn did clicked")
+    }
+    
+    func setRightPositionBtn(){
+        
+        guard let rightPositionBtn = rightPositionBtn else {
+            return
+        }
+        
+        navigationView.addSubview(rightPositionBtn)
+        
+        rightPositionBtn.snp.makeConstraints{
+            $0.trailing.equalToSuperview().offset(-20)
+            $0.centerY.equalToSuperview()
+            $0.width.equalTo(rightPositionBtn.snp.height)
+        }
+        
+        rightPositionBtn.addTarget(self, action: #selector(rightPositionBtnDidClicked), for: .touchUpInside)
+    }
+}
 
-
+extension BaseViewController{
+    
+    class EtcButton: UIButton{
+        
+        init(title: String){
+            super.init(frame: .zero)
+            
+            self.setTitle(title, for: .normal)
+            self.titleLabel?.textAlignment = .center
+            self.titleLabel?.font = UIFont.pretendard(size: 16, family: .Bold)
+            self.setTitleColor(.black85, for: .normal)
+            self.titleEdgeInsets = UIEdgeInsets(top: 5, left: 0, bottom: 4, right: 0)
+        }
+        
+        init(image: UIImage){
+            super.init(frame: .zero)
+            self.setImage(image, for: .normal)
+        }
+        
+        required init?(coder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+    }
 }
