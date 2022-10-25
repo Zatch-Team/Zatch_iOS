a3cf00012d21e8ecdd47aa68436ed05088156f04
gom1n <alice436777@gmail.com>
2022-10-26 02:32:23 +0900

    [feat] 공지사항 UI 구현

---
+++ Zatch/domain/ViewControllers/Notice/NoticeDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NoticeDetailViewController.swift
+//  Zatch
+//
+//  Created by gomin on 2022/10/26.
+//
+
+import UIKit
+
+class NoticeDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
