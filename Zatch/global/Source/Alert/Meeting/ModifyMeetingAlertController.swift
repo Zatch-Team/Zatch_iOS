d5f20fb630b1c21c27a02df35bd07d2f9abd7f47
Ssooya <bsy24660@naver.com>
2022-10-31 17:42:57 +0900

    #119: 재치 약속 잡기 Alert 파일 추가

---
+++ Zatch/global/Source/Alert/Meeting/ModifyMeetingAlertController.swift
@@ -0,0 +1,29 @@
+//
+//  ModifyMeetingAlertController.swift
+//  Zatch
+//
+//  Created by 박지윤 on 2022/10/31.
+//
+
+import UIKit
+
+class ModifyMeetingAlertController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
