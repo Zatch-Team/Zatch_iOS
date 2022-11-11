04c169130b918ee26783541d53dbeb7930573dbf
Ssooya <bsy24660@naver.com>
2022-11-11 21:22:10 +0900

    refactor: Kakao 로그인 유저 정보 함수 선언 통해 가져오기

--- Zatch/domain/ViewControllers/Login/LoginViewController.swift
+++ Zatch/domain/ViewControllers/Login/LoginViewController.swift
@@ -28,8 +28,6 @@ class LoginViewController: UIViewController {
     }
     
     @objc func kakaoBtnDidClicked(){
-        print("kakao btn did clicked")
-        
         // 카카오톡 설치 여부 확인
         if (UserApi.isKakaoTalkLoginAvailable()) {
             UserApi.shared.loginWithKakaoTalk {(oauthToken, error) in
@@ -39,7 +37,7 @@ class LoginViewController: UIViewController {
                 else {
                     print("loginWithKakaoTalk() success.")
                     //TODO: oauthToken 처리
-                    self.getUserData()
+                    self.getUserDataFromKakao()
                 }
             }
         }else{
@@ -50,13 +48,13 @@ class LoginViewController: UIViewController {
                 else {
                     print("loginWithKakaoAccount() success.")
                     //TODO: oauthToken 처리
-                    self.getUserData()
+                    self.getUserDataFromKakao()
                 }
             }
         }
     }
     
-    func getUserData(){
+    func getUserDataFromKakao(){
         
         UserApi.shared.me() { (user, error) in
             if let error = error {
@@ -73,52 +71,47 @@ class LoginViewController: UIViewController {
                     
                     if scopes.count > 0 {
                         print("사용자에게 추가 동의를 받아야 합니다.")
-
-                        // OpenID Connect 사용 시
-                        // scope 목록에 "openid" 문자열을 추가하고 요청해야 함
-                        // 해당 문자열을 포함하지 않은 경우, ID 토큰이 재발급되지 않음
-                        // scopes.append("openid")
                         
                         //scope 목록을 전달하여 카카오 로그인 요청
                         UserApi.shared.loginWithKakaoAccount(scopes: scopes) { (_, error) in
                             if let error = error {
                                 print(error)
-                            }
-                            else {
+                            }else {
                                 UserApi.shared.me() { (user, error) in
                                     if let error = error {
                                         print(error)
                                     }
                                     else {
                                         print("me() success.")
-                                        
-                                        //do something
-                                        _ = user
-                                        print("닉네임",user?.kakaoAccount?.email)
-                                        print("이름",user?.kakaoAccount?.profile?.nickname)
-                                        print("성별",user?.kakaoAccount?.gender)
-                                        print("생일",user?.kakaoAccount?.birthday)
-                                        
-                                        let vc = MainViewController()
-                                        self.navigationController?.pushViewController(vc, animated: true)
+                                        self.getUserData(user: user)
                                     }
                                 }
                             }
                         }
                     }
                     else {
-                        print("닉네임",user.kakaoAccount?.email)
-                        print("이름",user.kakaoAccount?.profile?.nickname)
-                        print("성별",user.kakaoAccount?.gender)
-                        print("생일",user.kakaoAccount?.birthday)
                         print("사용자의 추가 동의가 필요하지 않습니다.")
-                        
-                        let vc = MainViewController()
-                        self.navigationController?.pushViewController(vc, animated: true)
+                        self.getUserData(user: user)
                     }
                 }
             }
         }
     }
+    
+    func getUserData(user: User?){
+        
+        guard let user = user else { return }
+        
+        print("닉네임",user.kakaoAccount?.email)
+        print("이름",user.kakaoAccount?.profile?.nickname)
+        print("성별",user.kakaoAccount?.gender)
+        print("생일",user.kakaoAccount?.birthday)
+        print("사용자의 추가 동의가 필요하지 않습니다.")
+        
+        let vc = MainViewController()
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    
 
 }
