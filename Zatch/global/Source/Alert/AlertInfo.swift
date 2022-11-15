8d34854fb01cf0a2c319131cc61a7c8a10ed78b4
Ssooya <bsy24660@naver.com>
2022-11-15 11:05:52 +0900

    #140: RegisterQuestionViewController Alert 리팩토링

--- Zatch/global/Source/Alert/AlertInfo.swift
+++ Zatch/global/Source/Alert/AlertInfo.swift
@@ -18,6 +18,12 @@ enum Alert{
     //ChattingRoom
     case ChattingRoomExit
     case Block(user: String)
+    
+    //Question
+    case QuestionCategory
+    case QuestionTitle
+    case QuestionContent
+    case QuestionRegister
 }
 
 struct AlertDescription{
@@ -39,6 +45,15 @@ extension Alert{
             
         case .ChattingRoomExit: return CancelAlertViewController(message: "채팅방을 나가시겠습니까?\n채팅방을 나가면 채팅 내역은 복구되지 않습니다.", btnTitle: "네, 확인했습니다.")
         case .Block(let user):  return CancelAlertViewController(message: "\(user)님을 차단하시겠습니까?\n더 이상의 대화가 불가합니다.", btnTitle: "네, 차단합니다.")
+        
+        case .QuestionCategory:
+            return InfoAlertViewController(message: "카테고리를 선택해주세요.")
+        case .QuestionTitle:
+            return InfoAlertViewController(message: "제목을 입력해주세요.")
+        case .QuestionContent:
+            return InfoAlertViewController(message: "내용을 입력해주세요.")
+        case .QuestionRegister:
+            return CancelAlertViewController(message: "1:1 문의를 정말 등록하시겠습니까?", btnTitle: "등록")
         }
     }
 }
