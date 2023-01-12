850952cc2ba1de7e9f19fbf59200ecb70dad54d9
Ssooya <bsy24660@naver.com>
2023-01-12 13:15:34 +0900

    fix : BaseCollectionViewCell required init 추가 통한 컴파일 오류 해결

--- Zatch/global/Source/Base/BaseCollectionViewCell.swift
+++ Zatch/global/Source/Base/BaseCollectionViewCell.swift
@@ -20,6 +20,10 @@ class BaseCollectionViewCell: UICollectionViewCell {
         layout()
     }
     
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     func style() { }
     
     func hierarchy(){
