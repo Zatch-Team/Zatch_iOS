d7f4ea3dc1cea0b930ac958dbe846ef97f2b9ea1
SSOOYA <bsy24660@naver.com>
2023-01-28 22:38:56 +0900

    #165: LeftNavigationHeaderView (왼쪽에 네비게이션 타이틀 위치한 헤더)

---
+++ Zatch/Global/Source/HeaderView/LeftNavigationTitleHeaderView.swift
@@ -0,0 +1,34 @@
+//
+//  LeftNavigationTitleHeaderView.swift
+//  Zatch
+//
+//  Created by 박소윤 on 2023/01/28.
+//
+
+import Foundation
+
+class LeftNavigationHeaderView: BaseHeaderView, HeaderNavigationTitle{
+    
+    internal let title: String
+    let navigationTitleLabel = UILabel().then{
+        $0.font = UIFont.pretendard(size: 20, family: .Bold)
+    }
+    
+    init(title: String){
+        self.title = title
+        super.init(frame: .zero)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func style() {
+        navigationTitleLabel.text = title
+    }
+    
+    override func layout() {
+        super.layout()
+        layoutNavigationTitle()
+    }
+}
