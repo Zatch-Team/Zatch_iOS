26fecfed741ef28d136c2880a2ed5e53b680565a
SSOOYA <bsy24660@naver.com>
2023-01-28 22:35:54 +0900

    #165: BaseCenterTitleViewController override 메서드 제거

--- Zatch/Global/Source/Base/ViewController/BaseCenterTitleViewController.swift
+++ Zatch/Global/Source/Base/ViewController/BaseCenterTitleViewController.swift
@@ -8,12 +8,5 @@
 import UIKit
 
 class BaseCenterTitleViewController: BaseViewController<CenterNavigationHeaderView, BaseView> {
-    
-    override init(headerView: CenterNavigationHeaderView, mainView: BaseView) {
-        super.init(headerView: headerView, mainView: mainView)
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
+
 }
