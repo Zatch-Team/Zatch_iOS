5fbc23296e12f73507b87f946f84cdbde0f266c3
SSOOYA <bsy24660@naver.com>
2023-02-06 22:48:02 +0900

    #165: RegisterImageDetailViewController base 생성자 및 HeaderView 활용으로 리팩토링

--- Zatch/Presentation/ViewControllers/Register/Zatch/RegisterImageDetailViewController.swift
+++ Zatch/Presentation/ViewControllers/Register/Zatch/RegisterImageDetailViewController.swift
@@ -7,22 +7,29 @@
 
 import UIKit
 
-class RegisterImageDetailViewController: BaseViewController<EtcButtonHeaderView, RegisterImageDetailView> {
+class RegisterImageDetailViewController: BaseViewController<EtcButtonHeaderView, ImageDetailView> {
     
     //MARK: - Properties
     
-    var imageDetailHandler: ((Bool) -> ())?
+    var completion: (() -> ())!
+    
+    init(){
+        super.init(headerView: EtcButtonHeaderView(title: "확인"),
+                   mainView: ImageDetailView())
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func initialize() {
-        
         super.initialize()
-        
         headerView.etcButton.addTarget(self, action: #selector(okBtnDidClicked), for: .touchUpInside)
     }
     
     //MARK: - Action
     @objc func okBtnDidClicked(){
-        self.imageDetailHandler!(true)
+        self.completion()
         self.navigationController?.popViewController(animated: true)
     }
 
