3305b7f8cd119d520d2c9f9ff3797f3b1a8b5d19
SSOOYA <bsy24660@naver.com>
2023-02-10 16:03:45 +0900

    #171: 라디오버튼 + 타이틀 컴포넌트 > RadioButtonView 생성

---
+++ Zatch/Global/Source/Component/RadioButtonView.swift
@@ -0,0 +1,57 @@
+//
+//  RadioButtonView.swift
+//  Zatch
+//
+//  Created by 박소윤 on 2023/02/10.
+//
+
+import Foundation
+
+extension ZatchComponent{
+    
+    class RadioButtonView: BaseView{
+        
+        var isSelected = false{
+            didSet{
+                radioButton.isSelected = isSelected
+                self.tag = isSelected ? Const.ViewTag.select : Const.ViewTag.unselect
+            }
+        }
+        
+        private let stackView = UIStackView().then{
+            $0.isUserInteractionEnabled = true
+            $0.spacing = 8
+            $0.axis = .horizontal
+        }
+        
+        private lazy var radioButton = UIButton().then{
+            $0.isUserInteractionEnabled = false
+            $0.setImage(Image.zatchUncheck, for: .normal)
+            $0.setImage(Image.zatchCheck, for: .selected)
+        }
+        
+        private let titleLabel = UILabel().then{
+            $0.setTypoStyleWithSingleLine(typoStyle: .medium14)
+            $0.textColor = .black85
+        }
+        
+        override func hierarchy() {
+            self.addSubview(stackView)
+            stackView.addArrangedSubview(radioButton)
+            stackView.addArrangedSubview(titleLabel)
+        }
+        
+        override func layout() {
+            stackView.snp.makeConstraints{
+                $0.top.leading.trailing.bottom.equalToSuperview()
+            }
+            radioButton.snp.makeConstraints{
+                $0.width.height.equalTo(24)
+            }
+        }
+        
+        func setTitle(_ title: String){
+            titleLabel.text = title
+        }
+    }
+}
