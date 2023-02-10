425e5f9394229a18a2d63030c81d26dd0b7f37c9
SSOOYA <bsy24660@naver.com>
2023-02-10 11:37:02 +0900

    #172: RegisterCategorySelectTableViewCell의 view 프로퍼티로 RegisterCategorySelectView 설정

--- Zatch/Presentation/Cells/RegisterCells/RegisterCategorySelectTableViewCell.swift
+++ Zatch/Presentation/Cells/RegisterCells/RegisterCategorySelectTableViewCell.swift
@@ -13,67 +13,28 @@ class RegisterCategorySelectTableViewCell: BaseTableViewCell {
     
     var isSubViewOpen = false{
         didSet{
-            arrowImage.isSelected = isSubViewOpen
+            mainView.arrowImage.isSelected = isSubViewOpen
         }
     }
-    
-    let categoryFrame = UIView()
-    let boundaryLine = UIView().then{
-        $0.backgroundColor = .black5
-    }
-    private let categoryText = UILabel().then{
-        $0.text = "카테고리 선택"
-        $0.setTypoStyleWithSingleLine(typoStyle: .medium14)
-    }
-    private lazy var arrowImage = UIButton().then{
-        $0.setImage(Image.arrowDown, for: .normal)
-        $0.setImage(Image.arrowUp, for: .selected)
-    }
+    private let mainView = RegisterCategorySelectView()
     
     override func hierarchy() {
-        
         super.hierarchy()
-        
-        baseView.addSubview(categoryFrame)
-        
-        categoryFrame.addSubview(boundaryLine)
-        categoryFrame.addSubview(categoryText)
-        categoryFrame.addSubview(arrowImage)
+        baseView.addSubview(mainView)
     }
     
     override func layout() {
-        
         super.layout()
-        
-        categoryFrame.snp.makeConstraints{ make in
-            make.leading.trailing.top.bottom.equalToSuperview()
-            make.height.equalTo(41)
-        }
-        
-        boundaryLine.snp.makeConstraints{ make in
-            make.height.equalTo(1)
-            make.bottom.equalToSuperview()
-            make.leading.equalToSuperview().offset(20)
-            make.trailing.equalToSuperview().offset(-20)
-        }
-        
-        categoryText.snp.makeConstraints{ make in
-            make.centerY.equalToSuperview()
-            make.leading.equalToSuperview().offset(36)
-        }
-        
-        arrowImage.snp.makeConstraints{ make in
-            make.width.height.equalTo(24)
-            make.trailing.equalToSuperview().offset(-24)
-            make.top.equalToSuperview().offset(12)
+        mainView.snp.makeConstraints{
+            $0.top.leading.trailing.bottom.equalToSuperview()
         }
     }
     
     func setDefaultTitle(_ title: String){
-        categoryText.text = title
+        mainView.categoryText.text = title
     }
     
     func setCategoryTitle(id: Int){
-        categoryText.text = ServiceType.Zatch.getCategoryFromCategories(at: id).title
+        mainView.categoryText.text = ServiceType.Zatch.getCategoryFromCategories(at: id).title
     }
 }
