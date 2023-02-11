b63f26f22daa2baeb7d97fa2e2dedeb7821bf909
SSOOYA <bsy24660@naver.com>
2023-02-11 16:53:12 +0900

    #171: Tag 카테고리로 타이틀 설정 메서드 추가

--- Zatch/Global/Source/Component/Tag/Tag.swift
+++ Zatch/Global/Source/Component/Tag/Tag.swift
@@ -90,22 +90,30 @@ extension ZatchComponent{
         }
         
         private func setNormalState(){
+            self.tag = ViewTag.normal
             self.textColor = colorType.textColor
             self.backgroundColor = colorType.backgroundColor
         }
         
         private func setDisabledState(){
+            self.tag = ViewTag.deselect
             self.textColor = colorType.disabledTextColor
             self.backgroundColor = colorType.disabledBackgroundColor
         }
         
         private func setSelectState(){
+            self.tag = ViewTag.select
             self.textColor = colorType.selectedTextColor
             self.backgroundColor = colorType.selectedBackgroundColor
         }
         
         func setTitle(_ title: String){
             self.text = title
+        }
+        
+        func setCategoryTitle(categoryId: Int){
+            let category = ServiceType.Zatch.getCategoryFromCategories(at: categoryId)
+            self.text = category.title
         }
     }
 }
