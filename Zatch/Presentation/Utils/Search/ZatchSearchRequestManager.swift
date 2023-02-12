e3123c8a2abe9092924625b7df774093e87252e0
SSOOYA <bsy24660@naver.com>
2023-02-12 16:03:27 +0900

    #176: ZatchSearchRequestManager 싱글톤 클래스 생성

---
+++ Zatch/Presentation/Utils/Search/ZatchSearchRequestManager.swift
@@ -0,0 +1,17 @@
+//
+//  ZatchSearchRequestManager.swift
+//  Zatch
+//
+//  Created by 박소윤 on 2023/02/12.
+//
+
+import Foundation
+
+class ZatchSearchRequestManager{
+    
+    static let shared = ZatchSearchRequestManager()
+    
+    func initialize(){
+        
+    }
+}
