960c333a2094ad0687e7b6fcc06d1333a66a3a62
SSOOYA <bsy24660@naver.com>
2023-02-15 21:59:50 +0900

    #176: SearchTagSheetViewController 리팩토링 > 자식 클래스에 있던 CollectionView 관련 메서드 부모 클래스로 이동

--- Zatch/Presentation/Views/SearchView/Sheet/SearchTagSheetViewController.swift
+++ Zatch/Presentation/Views/SearchView/Sheet/SearchTagSheetViewController.swift
@@ -7,39 +7,57 @@
 
 import UIKit
 
-class SearchTagSheetViewController: BaseBottomSheetViewController<String> {
+class SearchTagSheetViewController: BaseBottomSheetViewController<String>{
     
     //MARK: - Properties
     
-    var tagData : [String]!
-    
+    var tagData = ["타이머","안경닦이","호랑이 인형","램프","2022 달력", "미니 가습기","마우스 패드"]
     var currentTag: Int?
     
-    var selectCompleteHandelr: ((String, Int) -> ())? //선택한 Tag 데이터 전달
-
     //MARK: - UI
     
     let collectionView = UICollectionView(frame: .zero, collectionViewLayout: .init()).then{
-        
-        let flexLayout = LeftAlignCollectionViewFlowLayout()
-        flexLayout.minimumLineSpacing = 16
-        flexLayout.minimumInteritemSpacing = 8
-        
+        let flexLayout = CenterAlignCollectionViewFlowLayout().then{
+            $0.minimumLineSpacing = 12
+            $0.minimumInteritemSpacing = 8
+        }
         $0.collectionViewLayout = flexLayout
     }
 
     override func layout() {
-        
         super.layout()
-        
         self.view.addSubview(collectionView)
-        
-        collectionView.snp.makeConstraints{ make in
-            make.top.equalTo(self.titleLabel.snp.bottom).offset(55)
-            make.leading.equalToSuperview().offset(33)
-            make.trailing.equalToSuperview().offset(-33)
-            make.bottom.equalToSuperview().offset(-30)
+        collectionView.snp.makeConstraints{
+            $0.top.equalTo(self.titleLabel.snp.bottom).offset(20)
+            $0.leading.equalToSuperview().offset(35)
+            $0.trailing.equalToSuperview().offset(-35)
+            $0.bottom.equalToSuperview().offset(-30)
         }
+    }
+    override func initialize() {
+        super.initialize()
+        collectionView.dataSource = self
+        collectionView.delegate = self
     }
 }
 
+extension SearchTagSheetViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout{
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.tagData.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return BaseCollectionViewCell()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return SearchWantZatchByRegisterCollectionViewCell.getEstimatedSize(title: tagData[indexPath.row])
+    }
+    
+//    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+////        selectCompleteHandelr!(tagData[indexPath.row], indexPath.row)
+//        self.dismiss(animated: true, completion: nil)
+//    }
+
+}
