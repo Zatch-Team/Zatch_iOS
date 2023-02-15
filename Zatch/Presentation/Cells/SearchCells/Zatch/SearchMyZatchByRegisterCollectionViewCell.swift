bca25e452c6efb166860f4c0b1ab0e09145737fe
SSOOYA <bsy24660@naver.com>
2023-02-15 22:02:54 +0900

    #176: SearchMyZatchByRegisterCollectionViewCell SearchZatchByRegister 프로토콜 채택으로 클래스 구현 > 나의 재치 셀 역할

---
+++ Zatch/Presentation/Cells/SearchCells/Zatch/SearchMyZatchByRegisterCollectionViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SearchMyZatchByRegisterCollectionViewCell.swift
+//  Zatch
+//
+//  Created by 박지윤 on 2022/08/07.
+//
+
+import UIKit
+
+class SearchMyZatchByRegisterCollectionViewCell: BaseCollectionViewCell, SearchZatchByRegister {
+    
+    let tagLabel = ZatchComponent.YellowTag(configuration: .height31)
+    
+    override func hierarchy() {
+        super.hierarchy()
+        tagLabelHierarchy()
+    }
+    
+    override func layout() {
+        super.layout()
+        tagLabelLayout()
+    }
+    
+}
