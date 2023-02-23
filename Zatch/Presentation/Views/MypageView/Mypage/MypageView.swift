08c0bbb22cdc2732ead71d8c478b1d0b3520362a
SSOOYA <bsy24660@naver.com>
2023-02-23 12:59:02 +0900

    #182: MyPageView Base 활용 리팩토링 + cell register 임시 주석 처리

--- Zatch/Presentation/Views/MypageView/Mypage/MypageView.swift
+++ Zatch/Presentation/Views/MypageView/Mypage/MypageView.swift
@@ -8,45 +8,29 @@
 import Foundation
 import UIKit
 
-class MypageView: UIView {
-    //
-   
+class MyPageView: BaseView {
     
-    // MARK: - Functions
-    var mypageTableView : UITableView!
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
+    let tableView = UITableView().then{
+        // autoHeight
+        $0.rowHeight = UITableView.automaticDimension
+        $0.estimatedRowHeight = UITableView.automaticDimension
+        $0.showsVerticalScrollIndicator = false
+        $0.separatorStyle = .none
         
+        $0.register(cellType: MyInfoTableViewCell.self)
+//        $0.register(cellType: CertificationBannerTableViewCell.self)
+//        $0.register(cellType: MyZatchStatisticTableViewCell.self)
+//        $0.register(cellType: TownSettingTableViewCell.self)
+//        $0.register(cellType: MypageSettingTableViewCell.self)
     }
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+    
+    override func hierarchy() {
+        self.addSubview(tableView)
     }
-    // MARK: - Functions
-    func setUpTableView(dataSourceDelegate: UITableViewDelegate & UITableViewDataSource) {
-        mypageTableView = UITableView()
-        mypageTableView.then{
-            $0.delegate = dataSourceDelegate
-            $0.dataSource = dataSourceDelegate
-            $0.register(MyInfoTableViewCell.self, forCellReuseIdentifier: "MyInfoTableViewCell")
-            $0.register(CertificationBannerTableViewCell.self, forCellReuseIdentifier: "CertificationBannerTableViewCell")
-            $0.register(MyZatchStatisticTableViewCell.self, forCellReuseIdentifier: "MyZatchStatisticTableViewCell")
-            $0.register(TownSettingTableViewCell.self, forCellReuseIdentifier: "TownSettingTableViewCell")
-            $0.register(MypageSettingTableViewCell.self, forCellReuseIdentifier: "MypageSettingTableViewCell")
-            
-            // autoHeight
-            $0.rowHeight = UITableView.automaticDimension
-            $0.estimatedRowHeight = UITableView.automaticDimension
-            $0.showsVerticalScrollIndicator = false
-            $0.separatorStyle = .none
-        }
-    }
-    func setUpView() {
-        addSubview(mypageTableView)
-    }
-    func setUpConstraint() {
-        mypageTableView.snp.makeConstraints { make in
-            make.leading.trailing.top.bottom.equalToSuperview()
+    
+    override func layout() {
+        tableView.snp.makeConstraints{
+            $0.top.bottom.leading.trailing.equalToSuperview()
         }
     }
 }
