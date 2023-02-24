528267a6d1079cf1ed69987e1d997b16e251563b
SSOOYA <bsy24660@naver.com>
2023-02-24 20:42:51 +0900

    #182: SettingTitleTableViewCell Base 메서드 활용으로 리팩토링

--- Zatch/Presentation/Cells/MypageCells/Setting/SettingTitleTableViewCell.swift
+++ Zatch/Presentation/Cells/MypageCells/Setting/SettingTitleTableViewCell.swift
@@ -9,29 +9,27 @@ import UIKit
 
 class SettingTitleTableViewCell: BaseTableViewCell {
     
-    let titleLabel = UILabel().then{
+    private let titleLabel = UILabel().then{
         $0.text = "알림 설정"
         $0.font = UIFont.pretendard(size: 13, family: .Bold)
         $0.textColor = .black85
     }
+
+    override func hierarchy() {
+        super.hierarchy()
+        baseView.addSubview(titleLabel)
+    }
     
-    static let cellIdentifier = "SettingTitleTableViewCell"
-    
-    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-        super.init(style: style, reuseIdentifier: reuseIdentifier)
-        
-        self.baseView.addSubview(titleLabel)
-        
+    override func layout() {
+        super.layout()
         titleLabel.snp.makeConstraints{
             $0.top.equalToSuperview().offset(17)
             $0.centerY.equalToSuperview()
             $0.leading.equalToSuperview().offset(20)
         }
-        
     }
     
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+    func setTitle(_ title: String){
+        titleLabel.text = title
     }
-    
 }
