0004a2e73c39d8679f3e547457350e10c5eff40b
SSOOYA <bsy24660@naver.com>
2023-03-05 13:38:46 +0900

    #191: HTTPMethodURL 추가 및 user url 설정

---
+++ Zatch/Data/Services/Foundation/HTTPMethodURL.swift
@@ -0,0 +1,27 @@
+//
+//  HTTPMethodURL.swift
+//  Zatch
+//
+//  Created by 박소윤 on 2023/03/05.
+//
+
+import Foundation
+
+enum HTTPMethodURL {
+    
+    static let user = "/users"
+    
+    struct GET {
+    }
+    
+    struct POST {
+    }
+    
+    struct PUT {
+    }
+    
+    struct DELETE {
+    }
+    
+    
+}
