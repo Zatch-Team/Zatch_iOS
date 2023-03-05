4fef2da7b392e65b1205e628f6a36eb3d3743a50
SSOOYA <bsy24660@naver.com>
2023-03-05 12:46:38 +0900

    refactor: Kakao URL 설정 AppDelegate > SceneDelegate로 변경

--- Zatch/AppDelegate/AppDelegate.swift
+++ Zatch/AppDelegate/AppDelegate.swift
@@ -36,14 +36,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-    
-    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
-        if (AuthApi.isKakaoTalkLoginUrl(url)) {
-            return AuthController.handleOpenUrl(url: url)
-        }
-        return false
-    }
-
-
 }
 
