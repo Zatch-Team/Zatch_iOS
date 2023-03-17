4fd962e4ff82f51a7d3d6eb4c5a74ccf37b505e8
SSOOYA <bsy24660@naver.com>
2023-03-17 19:56:31 +0900

    #194: TagStlye 파일 생성 > TagColor,TagConfiguration 열거형 추가

--- Zatch/Global/Source/Component/Tag/TagStlye.swift
+++ Zatch/Global/Source/Component/Tag/TagStlye.swift
@@ -7,99 +7,17 @@
 
 import Foundation
 
-extension ZatchComponent.Tag{
+extension ZatchComponent{
     
     enum TagColor{
         case purple
         case yellow
     }
-    
-    enum TagType{
+
+    enum TagConfiguration{
         case height20
         case height25
         case height29
         case height31
-    }
-}
-
-extension ZatchComponent.Tag.TagColor{
-    
-    struct TagColorStyle{
-        let textColor: UIColor
-        let backgroundColor: UIColor
-        let selectedTextColor: UIColor = .white
-        let selectedBackgroundColor: UIColor
-        let disabledTextColor: UIColor
-        let disabledBackgroundColor: UIColor
-    }
-    
-    private var colorInfo: TagColorStyle{
-        switch self{
-        case .purple:
-            return TagColorStyle(textColor: .zatchPurple,
-                                 backgroundColor: .purple40,
-                                 selectedBackgroundColor: .zatchPurple,
-                                 disabledTextColor: .black20,
-                                 disabledBackgroundColor: .black10)
-        case .yellow:
-            return TagColorStyle(textColor: .zatchDeepYellow,
-                                 backgroundColor: .yellow40,
-                                 selectedBackgroundColor: .zatchDeepYellow,
-                                 disabledTextColor: .black20,
-                                 disabledBackgroundColor: .black10)
-        }
-    }
-    
-    var textColor: UIColor{
-        colorInfo.textColor
-    }
-    
-    var backgroundColor: UIColor{
-        colorInfo.backgroundColor
-    }
-    
-    var selectedTextColor: UIColor{
-        colorInfo.selectedTextColor
-    }
-    
-    var selectedBackgroundColor: UIColor{
-        colorInfo.selectedBackgroundColor
-    }
-    
-    var disabledTextColor: UIColor{
-        colorInfo.disabledTextColor
-    }
-    
-    var disabledBackgroundColor: UIColor{
-        colorInfo.disabledBackgroundColor
-    }
-}
-
-extension ZatchComponent.Tag.TagType{
-    
-    var padding: ZatchComponent.Padding{
-        switch self{
-        case .height20:      return ZatchComponent.Padding(left: 8, right: 8, top: 0, bottom: 0)
-        case .height25:      return ZatchComponent.Padding(left: 12, right: 12, top: 2, bottom: 2)
-        case .height29:      return ZatchComponent.Padding(left: 10, right: 10, top: 4, bottom: 4)
-        case .height31:      return ZatchComponent.Padding(left: 12, right: 12, top: 6, bottom: 6)
-        }
-    }
-    
-    var font: TypoStyle{
-        switch self{
-        case .height20:                 return .medium12
-        case .height25, .height29:      return .medium15_21
-        case .height31:                 return .medium16
-        }
-    }
-    
-    var height: CGFloat{
-        switch self{
-        case .height20:      return 20
-        case .height25:      return 25
-        case .height29:      return 29
-        case .height31:      return 31
-        }
     }
 }
