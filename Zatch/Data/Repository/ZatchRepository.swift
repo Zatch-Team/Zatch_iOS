b833b4e1286d1a6deea34bef678d444992a7462e
SSOOYA <bsy24660@naver.com>
2023-03-18 19:36:22 +0900

    #203: 재치 API 위한 Repository 추가

---
+++ Zatch/Data/Repository/ZatchRepository.swift
@@ -0,0 +1,20 @@
+//
+//  ZatchRepository.swift
+//  Zatch
+//
+//  Created by 박소윤 on 2023/03/18.
+//
+
+import Foundation
+
+final class ZatchRepository: ZatchRepositoryInterface {
+    func getPopularZatch(){
+        
+    }
+    func getAroundZatch(){
+        
+    }
+    func fetchHeartState(){
+        
+    }
+}
