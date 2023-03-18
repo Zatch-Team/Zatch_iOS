df2a105b9fbb3dab77a593a0e71d3d30ba080a68
SSOOYA <bsy24660@naver.com>
2023-03-18 19:49:38 +0900

    #203: MainViewModel 프로퍼티 및 메서드 재설계

--- Zatch/Presentation/ViewModels/Main/MainViewModel.swift
+++ Zatch/Presentation/ViewModels/Main/MainViewModel.swift
@@ -11,13 +11,51 @@ import RxCocoa
 
 class MainViewModel: BaseViewModel{
     
+    var currentTownIndex = BehaviorSubject(value: 0)
+    var myTowns: [String] = ["상현동", "성복동", "풍덕천동"]
+    var aroundZatch = [Int](repeating: 0, count: 10)
+    var popularZatch = [Int](repeating: 0, count: 3)
+    
     struct Input{
     }
     
     struct Output{
+        let currentTown: Driver<String>
+    }
+    
+    private let aroundZatchUseCase: AroundZatchUseCaseInterface
+    private let popularZatchUseCase: PopularZatchUseCaseInterface
+//    private let heartZatchUseCase: HeartZatchUseCaseInterface
+    private let myTownUseCase: GetMyTownUseCaseInterface
+    
+    init(aroundZatchUseCase: AroundZatchUseCaseInterface = AroundZatchUseCase(),
+         popularZatchUseCase: PopularZatchUseCaseInterface = PopularZatchUseCase(),
+//         heartZatchUseCase: HeartZatchUseCaseInterface = HeartZatchUseCase(),
+         myTownUseCase: GetMyTownUseCaseInterface = GetMyTownUseCase()){
+        self.aroundZatchUseCase = aroundZatchUseCase
+        self.popularZatchUseCase = popularZatchUseCase
+//        self.heartZatchUseCase = heartZatchUseCase
+        self.myTownUseCase = myTownUseCase
     }
     
     func transform(_ input: Input) -> Output {
-        return Output()
+        let currentTown = currentTownIndex
+            .map{
+                self.myTowns[$0]
+            }.asDriver(onErrorJustReturn: "")
+        
+        return Output(currentTown: currentTown)
     }
+    
+    func changeCurrentTown(index: Int){
+        currentTownIndex.onNext(index)
+    }
+    
+//    func viewWillAppear(){
+//
+//    }
+//
+//    func viewWillDisappear(){
+//
+//    }
 }
