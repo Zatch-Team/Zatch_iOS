34d7227e92f733f1a484d22c4143063d992bac11
SSOOYA <bsy24660@naver.com>
2023-03-18 19:35:47 +0900

    #203: 내 동네 조회 Repository / UseCase 추가

---
+++ RepositoryInterface/UserRepositoryInterface.swift
@@ -0,0 +1,12 @@
+//
+//  UserRepositoryInterface.swift
+//  Zatch
+//
+//  Created by 박소윤 on 2023/03/18.
+//
+
+import Foundation
+
+protocol UserRepositoryInterface{
+    func getMyTowns()
+}
---
+++ Zatch/Data/Repository/UserRepository.swift
@@ -0,0 +1,14 @@
+//
+//  UserRepository.swift
+//  Zatch
+//
+//  Created by 박소윤 on 2023/03/18.
+//
+
+import Foundation
+
+final class UserRepository: UserRepositoryInterface {
+    func getMyTowns(){
+        
+    }
+}
