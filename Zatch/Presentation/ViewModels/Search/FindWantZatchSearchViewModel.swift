8b8d35c42816077ae0cd242ea1ed518c0ace01e4
SSOOYA <bsy24660@naver.com>
2023-03-20 21:57:44 +0900

    #206: FindWantZatchSearchViewModel 설계 및 적용

---
+++ Zatch/Presentation/ViewModels/Search/FindWantZatchSearchViewModel.swift
@@ -0,0 +1,88 @@
+//
+//  FindWantZatchSearchViewModel.swift
+//  Zatch
+//
+//  Created by 박소윤 on 2023/03/20.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+class FindWantZatchSearchViewModel: BaseViewModel{
+    
+    private var searchPopularKeywords = ["몰랑이","몰랑몰랑","몰랑"] //최대 3개
+    private var lookingForZatches = ["몰랑이","몰랑몰랑","몰랑"] //최대 3개
+    
+    private let searchManager = ZatchSearchRequestManager.shared
+    
+    private let wantProductName = PublishSubject<String>()
+    private let disposeBag = DisposeBag()
+    
+    private let popularKeywordUseCase: PopularKeywordUseCaseInterface
+    private let lookingForZatchUseCase: LookingForZatchUseCaseInterface
+
+    init(popularKeywordUseCase: PopularKeywordUseCaseInterface = PopularKeywordUseCase(),
+         lookingForZatchUseCase: LookingForZatchUseCaseInterface = LookingForZatchUseCase()){
+        self.popularKeywordUseCase = popularKeywordUseCase
+        self.lookingForZatchUseCase = lookingForZatchUseCase
+    }
+    
+    struct Input{
+        let productNameTextField: Observable<String>
+    }
+    
+    struct Output{
+        let searchProduct: Driver<String>
+    }
+    
+    func transform(_ input: Input) -> Output {
+        
+        input.productNameTextField
+            .subscribe{
+                self.wantProductName.onNext($0)
+            }.disposed(by: disposeBag)
+        
+        wantProductName
+            .subscribe(onNext: {
+                self.searchManager.wantProduct = $0
+            }).disposed(by: disposeBag)
+        
+        let productName = wantProductName
+            .asDriver(onErrorJustReturn: "")
+        
+        return Output(searchProduct: productName)
+    }
+    
+}
+
+extension FindWantZatchSearchViewModel{
+    
+    func getPopularKeywordsCount() -> Int{
+        searchPopularKeywords.count
+    }
+    
+    func getLookingForZatchCount() -> Int{
+        lookingForZatches.count
+    }
+    
+    func getPopularKeyword(at: Int) -> String{
+        searchPopularKeywords[at]
+    }
+    
+    func getLookingForZatch(at: Int) -> String{
+        lookingForZatches[at]
+    }
+    
+    func selectPopularKeyword(at index: Int){
+        wantProductName.onNext(searchPopularKeywords[index])
+    }
+    
+    func selectLookingForZatch(at index: Int){
+        wantProductName.onNext(lookingForZatches[index])
+    }
+    
+    func deselectCell(){
+        wantProductName.onNext("")
+    }
+}
