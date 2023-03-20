21e8a05269a821e1253552f03f1b06987332d28c
SSOOYA <bsy24660@naver.com>
2023-03-20 19:11:24 +0900

    #206: SearchRepository, SearchRepositotyInterface 추가

---
+++ Zatch/Domain/RepositoryInterface/SearchRepositotyInterface.swift
@@ -0,0 +1,12 @@
+//
+//  SearchRepositotyInterface.swift
+//  Zatch
+//
+//  Created by 박소윤 on 2023/03/20.
+//
+
+import Foundation
+
+protocol SearchRepositotyInterface{
+    func getPopularKeyword()
+}
