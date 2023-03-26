edca1f14651660c2a32b9bb138d9d7663adbeadb
SSOOYA <bsy24660@naver.com>
2023-03-26 14:56:30 +0900

    chore: Foundation 디렉토리 위치 변경

--- Zatch/Data/Foundation/HTTPMethodURL.swift
+++ Zatch/Data/Foundation/HTTPMethodURL.swift
@@ -10,8 +10,13 @@ import Foundation
 enum HTTPMethodURL {
     
     static let user = "/users"
+    static let kakaoLocal = "/v2/local"
     
     struct GET {
+        //KAKAO LOCAL
+        static let placeSearch = "/search/keyword.json"
+        static let townLocation = "/geo/coord2regioncode.json"
+        static let meetingLocation = "/geo/coord2address.json"
     }
     
     struct POST {
