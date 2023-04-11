9171b602c79589bca38fe4707696b34370a71c8e
SSOOYA <bsy24660@naver.com>
2023-04-11 23:54:33 +0900

    #218: Register Util 구조체 ProductOpenState 열거형 추가

--- Zatch/Presentation/Utils/Register/Register.swift
+++ Zatch/Presentation/Utils/Register/Register.swift
@@ -14,5 +14,20 @@ struct Register{
         case end = "유통기한"
     }
     
+    @frozen
+    enum ProductOpenState: Int{
+        case open = 1
+        case unopen = 2
+    }
+    
     typealias DateString = (year: String, month: String, date: String)
+}
+
+extension Register.ProductOpenState{
+    var title: String{
+        switch self{
+        case .unopen:   return "미개봉"
+        case .open:     return "개봉"
+        }
+    }
 }
