3bd41e54190a6d49856949c3cd386d2edabe94c8
SSOOYA <bsy24660@naver.com>
2023-04-13 21:18:20 +0900

    #218: ZatchRegisterSecondView 라디오 버튼 UI 제거 및 리팩토링 진행

--- Zatch/Presentation/Views/RegisterView/ZatchRegisterSecondView.swift
+++ Zatch/Presentation/Views/RegisterView/ZatchRegisterSecondView.swift
@@ -11,29 +11,19 @@ class ZatchRegisterSecondView: BaseView {
     
     //MARK: - UI
     
-    let topTitleView = TopTitleView(title: "받고 싶은\n물건이 있나요?")
+    private let topTitleView = TopTitleView(title: "받고 싶은\n물건이 있나요?")
     
-    var tableView = UITableView().then{
+    let tableView = UITableView().then{
         $0.showsVerticalScrollIndicator = false
-        
-        $0.register(cellType: RegisterCategorySelectWithPriorityTableViewCell.self)
+        $0.isScrollEnabled = Device.isSmallDevice ? true : false
+        $0.contentInset = UIEdgeInsets(top: -10, left: 0, bottom: 0, right: 0)
+        $0.register(cellType: RegisterPriorityTableViewCell.self)
+        $0.register(cellType: RegisterCategorySelectTableViewCell.self)
         $0.register(cellType: TextFieldTabeViewCell.self)
+        $0.register(cellType: AnyZatchSelectTableViewCell.self)
     }
     
-    let radioButtonFrame = UIStackView().then{
-        $0.spacing = 16
-        $0.axis = .vertical
-    }
-    
-    let topRadioButtonFrame = ZatchComponent.RadioButtonView().then{
-        $0.setTitle("다른 것도 괜찮아요!")
-        $0.isSelected = true
-    }
-    let belowRadioButtonFrame = ZatchComponent.RadioButtonView().then{
-        $0.setTitle("이 것만 가능해요!")
-    }
-    
-    let btnFrame = UIStackView().then{
+    private let btnFrame = UIStackView().then{
         $0.spacing = 10
         $0.axis = .horizontal
     }
@@ -44,28 +34,11 @@ class ZatchRegisterSecondView: BaseView {
     
     lazy var nextBtn = Purple36Button(title: "다음 단계로")
     
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        initialize()
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    private func initialize() {
-        topRadioButtonFrame.isSelected = true
-    }
-
     override func hierarchy() {
         
-        self.addSubview(topTitleView)
-        self.addSubview(tableView)
-        self.addSubview(radioButtonFrame)
-        self.addSubview(btnFrame)
-        
-        self.radioButtonFrame.addArrangedSubview(topRadioButtonFrame)
-        self.radioButtonFrame.addArrangedSubview(belowRadioButtonFrame)
+        addSubview(topTitleView)
+        addSubview(tableView)
+        addSubview(btnFrame)
         
         btnFrame.addArrangedSubview(shareBtn)
         btnFrame.addArrangedSubview(nextBtn)
@@ -73,30 +46,25 @@ class ZatchRegisterSecondView: BaseView {
     
     override func layout() {
         
-        topTitleView.snp.makeConstraints{ make in
-            make.top.equalToSuperview()
-            make.leading.trailing.equalToSuperview()
+        topTitleView.snp.makeConstraints{
+            $0.top.equalToSuperview()
+            $0.leading.trailing.equalToSuperview()
         }
         
-        tableView.snp.makeConstraints{ make in
-            make.top.equalTo(topTitleView.snp.bottom).offset(-11)
-            make.leading.trailing.equalToSuperview()
+        tableView.snp.makeConstraints{
+            $0.top.equalTo(topTitleView.snp.bottom).offset(-11)
+            $0.leading.trailing.equalToSuperview()
+            $0.bottom.equalTo(btnFrame).inset(10)
         }
         
-        radioButtonFrame.snp.makeConstraints{ make in
-            make.top.equalTo(tableView.snp.bottom).offset(36)
-            make.leading.equalToSuperview().offset(24)
+        btnFrame.snp.makeConstraints{
+            $0.leading.equalToSuperview().offset(25)
+            $0.trailing.equalToSuperview().offset(-15)
+            $0.bottom.equalToSuperview().offset(-48)
         }
         
-        btnFrame.snp.makeConstraints{ make in
-            make.leading.equalToSuperview().offset(25)
-            make.trailing.equalToSuperview().offset(-15)
-            make.bottom.equalToSuperview().offset(-48)
-            make.top.equalTo(radioButtonFrame.snp.bottom).offset(68) //68
-        }
-        
-        nextBtn.snp.makeConstraints{ make in
-            make.width.equalTo(shareBtn)
+        nextBtn.snp.makeConstraints{
+            $0.width.equalTo(shareBtn)
         }
     }
 }
