649b407822b4fe12646858abd4413ca2e27c2dd1
SSOOYA <bsy24660@naver.com>
2023-04-17 13:38:33 +0900

    #222: CheckRegisterViewModel 파일 생성 및 설계

---
+++ Zatch/Presentation/ViewModels/Register/CheckRegisterViewModel.swift
@@ -0,0 +1,69 @@
+//
+//  CheckRegisterViewModel.swift
+//  Zatch
+//
+//  Created by 박소윤 on 2023/04/17.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+class CheckRegisterViewModel: BaseViewModel{
+    
+    private let disposeBag = DisposeBag()
+    
+    struct Input{
+        let myProductInfo: RegisterFirstInformationDTO
+        let wantProductInfo: RegisterSecondInformationDTO
+        let comment: Observable<String>
+        let registerButtonTap: Observable<Void>
+    }
+    
+    struct Output{
+//        let registerRequestStatus: Driver<Void>
+    }
+    
+    func transform(_ input: Input) -> Output {
+        let commentObservable = input.comment
+            .map{
+                $0 == CheckRegisterView.placeholder ? "" : $0
+            }
+        
+//        let registerRequestStatus =
+        input.registerButtonTap
+            .withLatestFrom(commentObservable)
+            .map{ [self] in
+                makeRequestModel(
+                    myProduct: input.myProductInfo,
+                    wantProduct: input.wantProductInfo,
+                    comment: $0
+                )
+            }
+            .subscribe(onNext: {
+                print($0)
+            })
+        
+        return Output()
+    }
+    
+    private func makeRequestModel(myProduct: RegisterFirstInformationDTO, wantProduct: RegisterSecondInformationDTO, comment: String) -> RegisterZatchRequestModel{
+        return RegisterZatchRequestModel(
+            anyZatch: wantProduct.wantProductType,
+            categoryId: myProduct.category,
+            content: comment,
+            expirationDate: myProduct.endDate,
+            isFree: wantProduct.isFree,
+            isOpened: myProduct.isOpen,
+            itemName: myProduct.productName,
+            purchaseDate: myProduct.buyDate,
+            quantity: myProduct.count,
+            priorites: getPriorites(wantProduct),
+            userId: 0
+        )
+    }
+    
+    private func getPriorites(_ wantProduct: RegisterSecondInformationDTO) -> [RegisterZatchRequestModel.WantProductPriority]{
+        wantProduct.isFree ? [] : [wantProduct.firstPriority, wantProduct.secondPriority, wantProduct.thirdPriority].compactMap{ $0 }
+    }
+}
