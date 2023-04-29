8df051aaa93170d5b299ec59f54acfd0332aa7ac
SSOOYA <bsy24660@naver.com>
2023-04-29 20:05:17 +0900

    feat: UserManager 통한 UserDefault 관리

---
+++ Zatch/Global/Source/UserManager/UserDefaultKey.swift
@@ -0,0 +1,14 @@
+//
+//  UserDefaultKey.swift
+//  Zatch
+//
+//  Created by 박소윤 on 2023/04/29.
+//
+
+import Foundation
+
+struct UserDefaultKey{
+    static let userId = "userId"
+    static let token = "token"
+    static let nickName = "nickName"
+}
