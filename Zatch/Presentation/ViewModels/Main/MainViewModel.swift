3170fbec50efca0718c7aa5219fe62e75eb8aae0
SSOOYA <bsy24660@naver.com>
2023-05-11 13:09:11 +0900

    #234: MainViewModel ZatchLikeViewModel 합성 > 좋아요 기능 요청/응답 로직 구현

--- Zatch/Presentation/ViewModels/Main/MainViewModel.swift
+++ Zatch/Presentation/ViewModels/Main/MainViewModel.swift
@@ -9,14 +9,26 @@ import Foundation
 import RxSwift
 import RxCocoa
 
-class MainViewModel: BaseViewModel{
+protocol MainViewModelInterface{
+    var aroundZatch: [(Int, Bool)] { get }
+    var popularZatch: [(Int, Bool)] { get }
+}
+
+protocol ZatchLikeViewModelInterface{
+    func changeAroundZatchState(_ state: Bool, index: Int)
+    func changePopularZatchState(_ state: Bool, index: Int)
+}
+
+class MainViewModel: MainViewModelInterface, ZatchLikeViewModelInterface, BaseViewModel{
+    
+    var aroundZatch = [(Int, Bool)](repeating: (0, false), count: 10)
+    var popularZatch = [(Int, Bool)](repeating: (0, false), count: 10)
     
     private var currentTownIndex = BehaviorSubject(value: 0)
     private var myTowns: [String] = ["상현동", "성복동", "풍덕천동"]
-    private var aroundZatch = [Int](repeating: 0, count: 10)
-    private var popularZatch = [Int](repeating: 0, count: 3)
     
     struct Input{
+        
     }
     
     struct Output{
@@ -25,18 +37,20 @@ class MainViewModel: BaseViewModel{
     
     private let aroundZatchUseCase: AroundZatchUseCaseInterface
     private let popularZatchUseCase: PopularZatchUseCaseInterface
-//    private let heartZatchUseCase: HeartZatchUseCaseInterface
     private let myTownUseCase: GetMyTownUseCaseInterface
+    
+    private let likeViewModel: ZatchLikeViewModel
     
     init(aroundZatchUseCase: AroundZatchUseCaseInterface = AroundZatchUseCase(),
          popularZatchUseCase: PopularZatchUseCaseInterface = PopularZatchUseCase(),
-//         heartZatchUseCase: HeartZatchUseCaseInterface = HeartZatchUseCase(),
          myTownUseCase: GetMyTownUseCaseInterface = GetMyTownUseCase()){
         self.aroundZatchUseCase = aroundZatchUseCase
         self.popularZatchUseCase = popularZatchUseCase
-//        self.heartZatchUseCase = heartZatchUseCase
         self.myTownUseCase = myTownUseCase
+        self.likeViewModel = ZatchLikeViewModel()
     }
+    
+    private let disposeBag = DisposeBag()
     
     func transform(_ input: Input) -> Output {
         let currentTown = currentTownIndex
@@ -47,13 +61,31 @@ class MainViewModel: BaseViewModel{
         return Output(currentTown: currentTown)
     }
     
-//    func viewWillAppear(){
-//
-//    }
-//
-//    func viewWillDisappear(){
-//
-//    }
+    func changeAroundZatchState(_ state: Bool, index: Int) {
+        
+        let input = ZatchLikeViewModel.Input(zatchId: aroundZatch[index].0, heartState: state)
+        
+        let output = likeViewModel.transform(input)
+        output.heartState
+            .subscribe(onNext: {
+                if let data = $0 {
+                    self.aroundZatch[index].1 = data.isHeart
+                }
+            }).disposed(by: disposeBag)
+    }
+        
+    func changePopularZatchState(_ state: Bool, index: Int) {
+        
+        let input = ZatchLikeViewModel.Input(zatchId: popularZatch[index].0, heartState: state)
+        
+        let output = likeViewModel.transform(input)
+        output.heartState
+            .subscribe(onNext: {
+                if let data = $0 {
+                    self.popularZatch[index].1 = data.isHeart
+                }
+            }).disposed(by: disposeBag)
+    }
 }
 
 //MARK: - Method
@@ -73,13 +105,5 @@ extension MainViewModel{
     
     func myTownCount() -> Int{
         myTowns.count
-    }
-    
-    func aroundZatchCount() -> Int{
-        aroundZatch.count
-    }
-    
-    func popularZatchCount() -> Int{
-        popularZatch.count
     }
 }
