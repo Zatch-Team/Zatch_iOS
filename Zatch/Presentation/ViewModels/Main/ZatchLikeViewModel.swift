75b78912b21a127907dd1e69ba69336bedab3d44
SSOOYA <bsy24660@naver.com>
2023-05-11 13:04:59 +0900

    #234: ZatchLikeViewModel 설계 및 구현 (좋아요 기능만 수행)

---
+++ Zatch/Presentation/ViewModels/Main/ZatchLikeViewModel.swift
@@ -0,0 +1,49 @@
+//
+//  ZatchLikeViewModel.swift
+//  Zatch
+//
+//  Created by 박소윤 on 2023/01/24.
+//
+
+import Foundation
+import RxCocoa
+import RxSwift
+
+class ZatchLikeViewModel: BaseViewModel{
+    
+    private let likeUseCaseInterface: ZatchLikeUseCaseInterface
+    private let dislikeUseCaseInterface: ZatchDislikeUseCaseInterface
+    
+    init(likeUseCaseInterface: ZatchLikeUseCaseInterface = ZatchLikeUseCase(),
+         dislikeUseCaseInterface: ZatchDislikeUseCaseInterface = ZatchDisLikeUseCase()){
+        self.likeUseCaseInterface = likeUseCaseInterface
+        self.dislikeUseCaseInterface = dislikeUseCaseInterface
+    }
+    
+    struct Input{
+        let zatchId: Int
+        let heartState: Bool
+    }
+    
+    struct Output{
+        let heartState: Observable<ZatchLikeResponseModel?>
+    }
+    
+    
+    func transform(_ input: Input) -> Output{
+    
+        let requestValue = ZatchLikeRequestValue(zatchId: input.zatchId)
+        
+        let response = Observable.just(input.heartState)
+            .map{
+                !$0
+            }
+            .flatMap{
+                $0
+                ? self.likeUseCaseInterface.execute(requestValue: requestValue)
+                : self.dislikeUseCaseInterface.execute(requestValue: requestValue)
+            }
+
+        return Output(heartState: response)
+    }
+}
