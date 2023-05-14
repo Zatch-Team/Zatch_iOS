cda188f4e9d671e9e6202e73889771baad863799
SSOOYA <bsy24660@naver.com>
2023-05-14 17:16:49 +0900

    refactor: 재치 등록 task 헬퍼 함수 활용(모듈화)

--- Zatch/Data/MoyaService/Zatch/ZatchRouter.swift
+++ Zatch/Data/MoyaService/Zatch/ZatchRouter.swift
@@ -20,7 +20,7 @@ extension ZatchRouter{
         switch self {
         case .likeZatch(let requestValue):          return HTTPMethodURL.POST.likeZatch + String(requestValue.zatchId) + "/likes"
         case .dislikeZatch(let requestValue):       return HTTPMethodURL.DELETE.dislikeZatch + String(requestValue.zatchId) + "/dislikes"
-        case .registerZatch:      return HTTPMethodURL.POST.zatch
+        case .registerZatch:                        return HTTPMethodURL.POST.zatch
         }
     }
     
@@ -33,29 +33,34 @@ extension ZatchRouter{
     
     var task: Task {
         switch self {
-        case .likeZatch, .dislikeZatch:         return .requestPlain
-        case .registerZatch(let request, let images):
-            
-            var multiPartFormData = [MultipartFormData]()
- 
-            guard let parameters = request.toDictionary else { fatalError() }
-            
-            for (key, value) in parameters {
-                multiPartFormData.append(MultipartFormData(provider: .data("\(value)".data(using: .utf8)!),
-                                                           name: key))
-            }
-            
-            for image in images {
-                let imageData = image.jpegData(compressionQuality: 1.0)
-                let multipartImage = MultipartFormData(provider: .data(imageData!),
-                                                       name: "image",
-                                                       fileName: "image",
-                                                       mimeType: "image/jpeg")
-                
-                multiPartFormData.append(multipartImage)
-            }
-            
-            return .uploadMultipart(multiPartFormData)
+        case .likeZatch, .dislikeZatch:                     return .requestPlain
+        case .registerZatch(let request, let images):       return .uploadMultipart(getFormData(requestModel: request, images: images))
+        }
+    }
+    
+    private func getFormData(requestModel: RegisterZatchRequestModel, images: [UIImage]) -> [MultipartFormData]{
+        var multiPartFormData = [MultipartFormData]()
+        setExceptImages(requestModel, formData: &multiPartFormData)
+        setImages(images, formData: &multiPartFormData)
+        return multiPartFormData
+    }
+    
+    private func setExceptImages(_ requestModel: RegisterZatchRequestModel, formData: inout [MultipartFormData]){
+        guard let parameters = requestModel.toDictionary else { fatalError() }
+        for (key, value) in parameters {
+            formData.append(MultipartFormData(provider: .data("\(value)".data(using: .utf8)!),
+                                              name: key))
+        }
+    }
+    
+    private func setImages(_ images: [UIImage], formData: inout [MultipartFormData]){
+        for image in images {
+            let imageData = image.jpegData(compressionQuality: 1.0)
+            let multipartImage = MultipartFormData(provider: .data(imageData!),
+                                                   name: "image",
+                                                   fileName: "image",
+                                                   mimeType: "image/jpeg")
+            formData.append(multipartImage)
         }
     }
 }
