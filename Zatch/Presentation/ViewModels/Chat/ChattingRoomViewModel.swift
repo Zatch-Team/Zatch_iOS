a4af417cb4fad79bd6e6bdc7f8f7af001aa8642d
SSOOYA <bsy24660@naver.com>
2023-05-24 19:59:24 +0900

    [refactor] #245: 채팅방 ViewModel 일부 설계

---
+++ Zatch/Presentation/ViewModels/Chat/ChattingRoomViewModel.swift
@@ -0,0 +1,61 @@
+//
+//  ChattingRoomViewModel.swift
+//  Zatch
+//
+//  Created by 박소윤 on 2023/05/23.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+protocol ChattingRoomViewModelInterface: BaseViewModel, BlockUserInterface{
+    var messages: [ChatMessage] { get }
+    func viewDidLoad()
+}
+
+protocol BlockUserInterface{
+    var chattingmMembers: [String] { get }
+    var blockUserIndexSubject: PublishSubject<Int> { get }
+    var blockUserResponse: PublishSubject<ResponseState> { get }
+}
+
+class ChattingRoomViewModel: ChattingRoomViewModelInterface{
+    
+    var messages = [ChatMessage]()
+    var chattingmMembers = [String]()
+    
+    let blockUserIndexSubject = PublishSubject<Int>()
+    let blockUserResponse = PublishSubject<ResponseState>()
+    
+    private let blockUserUseCase: BlockUserUseCaseInterface
+    private let exitRoomUseCase: ExitChattingRoomUseCaseInterface
+    private let getChattingMemberListUseCase: GetMemberListOfChattingUseCaseInterface
+
+    init(blockUserUseCase: BlockUserUseCaseInterface = BlockUserUseCase(),
+         exitRoomUseCase: ExitChattingRoomUseCaseInterface = ExitChattingRoomUseCase(),
+         getChattingMemberListUseCase: GetMemberListOfChattingUseCaseInterface = GetMemberListOfChattingUseCase()) {
+        self.blockUserUseCase = blockUserUseCase
+        self.exitRoomUseCase = exitRoomUseCase
+        self.getChattingMemberListUseCase = getChattingMemberListUseCase
+    }
+
+    struct Input{
+//        let messageObservable: Observable<String>
+//        let sendBtnTap: ControlEvent<Void>
+//        let exitBtnTap: ControlEvent<Void>
+    }
+    
+    struct Output{
+//        let exitResponse: Observable<ResponseState>
+    }
+    
+    func transform(_ input: Input) -> Output{
+        Output()
+    }
+    
+    func viewDidLoad() {
+        //채팅방 멤버 조회
+        
+    }
+}
