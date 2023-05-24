327a36b0c99a698cd71d4385eb153ab9c82ae1d8
SSOOYA <bsy24660@naver.com>
2023-05-24 19:00:37 +0900

    [feat] #241: ChatRepository 설계 및 메서드 구현

---
+++ Zatch/Data/Repository/ChatRepository.swift
@@ -0,0 +1,73 @@
+//
+//  ChatRepository.swift
+//  Zatch
+//
+//  Created by 박소윤 on 2023/05/23.
+//
+
+import Foundation
+import RxSwift
+
+final class ChatRepository: ChatRepositoryInterface {
+
+    func getChattingRooms() -> Observable<[String]?> {
+        let observable = Observable<[String]?>.create { observer -> Disposable in
+            let requestReference: () = ChatService.shared.getChattingRoomList{ response in
+                switch response {
+                case .success(let data):
+                    observer.onNext(data)
+                case .failure:
+                    observer.onNext(nil)
+                }
+            }
+            return Disposables.create(with: { requestReference })
+        }
+        return observable
+    }
+    
+    
+    func blockUser(requestModel: BlockUserRequestModel) -> Observable<ResponseState> {
+        let observable = Observable<ResponseState>.create { observer -> Disposable in
+            let requestReference: () = ChatService.shared.blockUser(requestModel: requestModel){ response in
+                switch response {
+                case .success:
+                    observer.onNext(.success)
+                case .failure:
+                    observer.onNext(.fail)
+                }
+            }
+            return Disposables.create(with: { requestReference })
+        }
+        return observable
+    }
+
+    func getChattingMembers(roomId: String) -> Observable<[String]?> {
+        let observable = Observable<[String]?>.create { observer -> Disposable in
+            let requestReference: () = ChatService.shared.getChattingRoomMemberList(roomId: roomId){ response in
+                switch response {
+                case .success(let data):
+                    observer.onNext(data)
+                case .failure:
+                    observer.onNext(nil)
+                }
+            }
+            return Disposables.create(with: { requestReference })
+        }
+        return observable
+    }
+
+    func exitChattingRoom(roomId: String) -> Observable<ResponseState> {
+        let observable = Observable<ResponseState>.create { observer -> Disposable in
+            let requestReference: () = ChatService.shared.exitChattingRoom(roomId: roomId) { response in
+                switch response {
+                case .success:
+                    observer.onNext(.success)
+                case .failure:
+                    observer.onNext(.fail)
+                }
+            }
+            return Disposables.create(with: { requestReference })
+        }
+        return observable
+    }
+}
---
+++ Zatch/Domain/RepositoryInterface/ChatRepositoryInterface.swift
@@ -0,0 +1,17 @@
+//
+//  ChatRepositoryInterface.swift
+//  Zatch
+//
+//  Created by 박소윤 on 2023/05/23.
+//
+
+import Foundation
+import RxSwift
+
+protocol ChatRepositoryInterface{
+    func blockUser(requestModel: BlockUserRequestModel) -> Observable<ResponseState>
+    func getChattingMembers(roomId: String) -> Observable<[String]?>
+    func getChattingRooms() -> Observable<[String]?>
+    func exitChattingRoom(roomId: String) -> Observable<ResponseState>
+}
+
