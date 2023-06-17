cc32a7e17972413fbfc64ca394da46a20df6e37a
SSOOYA <bsy24660@naver.com>
2023-06-17 12:59:27 +0900

    [feat] : RadioButtonView Configuration 설정 추가

--- Zatch/Global/Source/Component/RadioButtonView.swift
+++ Zatch/Global/Source/Component/RadioButtonView.swift
@@ -9,6 +9,11 @@ import Foundation
 
 extension ZatchComponent{
     
+    struct RadioConfiguration{
+        let typo: TypoStyle
+        let imageSize: CGFloat
+    }
+    
     class RadioButtonView: BaseView{
         
         var isSelected = false{
@@ -18,25 +23,42 @@ extension ZatchComponent{
             }
         }
         
+        private let configuration: RadioConfiguration
+        
+        convenience init(){
+            self.init(configuration: RadioConfiguration(typo: .medium14, imageSize: 24), tag: 0)
+        }
+        
+        init(configuration: RadioConfiguration, tag: Int){
+            self.configuration = configuration
+            super.init(frame: .zero)
+            self.radioButton.tag = tag
+        }
+        
+        required init?(coder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+        
         private let stackView = UIStackView().then{
             $0.isUserInteractionEnabled = true
             $0.spacing = 8
             $0.axis = .horizontal
+            $0.alignment = .center
         }
         
-        private lazy var radioButton = UIButton().then{
-            $0.isUserInteractionEnabled = false
+        lazy var radioButton = UIButton().then{
+//            $0.isUserInteractionEnabled = false
             $0.setImage(Image.zatchUncheck, for: .normal)
             $0.setImage(Image.zatchCheck, for: .selected)
         }
         
-        private let titleLabel = UILabel().then{
-            $0.setTypoStyleWithSingleLine(typoStyle: .medium14)
+        private lazy var titleLabel = UILabel().then{
+            $0.setTypoStyleWithSingleLine(typoStyle: configuration.typo)
             $0.textColor = .black85
         }
         
         override func hierarchy() {
-            self.addSubview(stackView)
+            addSubview(stackView)
             stackView.addArrangedSubview(radioButton)
             stackView.addArrangedSubview(titleLabel)
         }
@@ -46,7 +68,7 @@ extension ZatchComponent{
                 $0.top.leading.trailing.bottom.equalToSuperview()
             }
             radioButton.snp.makeConstraints{
-                $0.width.height.equalTo(24)
+                $0.width.height.equalTo(configuration.imageSize)
             }
         }
         
